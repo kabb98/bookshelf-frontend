67fea7cb3ededab54d1e4899a3f737d7d4d97e88
Kenny <kennyberrones@gmail.com>
2024-04-30 00:27:58 +0200

    Rutas para los libros buscados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BooksComponent } from './components/books/books.component';
+import { SearchedBooksComponent } from './components/searched-books/searched-books.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'books', component: BooksComponent}
+    {path: 'books', component: BooksComponent},
+    {path: 'searched_books', component: SearchedBooksComponent}
 ];
