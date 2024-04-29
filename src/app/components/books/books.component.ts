70f1adbba47a4eaa0d07cacdb624b77f795a2fc9
Kenny <kennyberrones@gmail.com>
2024-04-30 00:30:08 +0200

    BooksApi Provider

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -2,19 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/books/books';
 import { BooksService } from '../../services/books/books.service';
 import { CommonModule } from '@angular/common';
+import { BooksApiService } from '../../services/books/books-api.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
-  providers: [BooksService],
+  providers: [BooksService, BooksApiService],
   imports: [CommonModule],
   standalone: true
 })
+
+
 export class BooksComponent implements OnInit {
   books: Book[] = [];
 
   constructor(private booksService: BooksService) { }
+
+
+
 
   ngOnInit() {
     this.booksService.getMyData().subscribe(books => {
