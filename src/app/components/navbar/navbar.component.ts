9f82b825a1fe8e5176f9f4568b7159163176d282
Kenny <kennyberrones@gmail.com>
2024-04-30 00:29:52 +0200

    Permite busqueda de libros

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BooksApiService } from '../../services/books/books-api.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, HttpClientModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
+  providers: [BooksApiService]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  searchValue: string = '';
+  booksSearched: any[] = [];
+  searchForm = this.fb.nonNullable.group(
+    {
+      searchValue: ''
+    }
+  );
+
+  constructor(
+    private booksApi: BooksApiService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  searchBooks(query: string) {
+    this.booksApi.getBooksApi(query).subscribe(booksFromApi => {
+      this.booksSearched = booksFromApi;
+      this.booksApi.updateBooksSearched(this.booksSearched);
+    });
+    this.router.navigate(['/searched_books']);
+  }
+
+  onSearchSubmit(): void {
+    this.searchValue = this.searchForm.value.searchValue ?? '';
+    this.searchBooks(this.searchValue);
+    this.booksApi.updateSearchValue(this.searchValue);
+  }
 }
